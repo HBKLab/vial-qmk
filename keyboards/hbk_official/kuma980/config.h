ee0a609f028ef1961290616f50d67dc04a44233f
HBK_Official <weixi17677148685@163.com>
2022-12-22 20:56:13 +0800

    添加kuma980代码

---
+++ keyboards/hbk_official/kuma980/config.h
@@ -0,0 +1,114 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xFEDD
+#define PRODUCT_ID      0x1111
+#define DEVICE_VER      0x1980
+#define MANUFACTURER    HBK_OFFicial
+#define PRODUCT         KUMA980
+
+/* key matrix size */
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 18
+
+#define MATRIX_ROW_PINS { B5, B4, B3, A15, A9 }
+#define MATRIX_COL_PINS { B15, B6, B7, B8, A0, A1, A2, A3 ,A4, A5, A6, B0 ,B1, B10, B11, B12, B13, B14 }
+
+#define DIODE_DIRECTION ROW2COL
+
+// #define FORCE_NKRO
+#define DEBOUNCE 5  //消抖
+#define FORCE_NKRO
+
+/* RGN Matrix */
+#ifdef RGB_MATRIX_ENABLE
+#    define RGB_DI_PIN   A7                       // 灯引脚
+#    define RGBLED_NUM   2                        // 灯数量
+#    define DRIVER_LED_TOTAL RGBLED_NUM   
+
+#    define WS2812_PWM_DRIVER PWMD3               // default: PWMD2  （TIMx）
+#    define WS2812_PWM_CHANNEL 2                  // default: 2      （_CHx）
+#    define WS2812_PWM_PAL_MODE 2                 // Pin "alternate function", see the respective datasheet for the appropriate values for your MCU. default: 2
+#    define WS2812_DMA_STREAM STM32_DMA1_STREAM3  // （TIMx_up对应的dma通道） DMA Stream for TIMx_UP, see the respective reference manual for the appropriate values for your MCU.
+#    define WS2812_DMA_CHANNEL 3                  // （就是CHx）  DMA Channel for TIMx_UP, see the respective reference manual for the appropriate values for your MCU.               
+
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 255     // 最大亮度限制
+#    define RGB_DISABLE_WHEN_USB_SUSPENDED true   // 休眠关闭灯效（大概）
+// #    define RGB_MATRIX_CENTER { 96, 32 }          // 中心点
+#    define RGB_MATRIX_KEYPRESSES
+#    define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+
+#   define ENABLE_RGB_MATRIX_ALPHAS_MODS
+#   define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+#   define ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
+#   define ENABLE_RGB_MATRIX_BREATHING
+#   define ENABLE_RGB_MATRIX_BAND_SAT
+#   define ENABLE_RGB_MATRIX_BAND_VAL
+#   define ENABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
+#   define ENABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
+#   define ENABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+#   define ENABLE_RGB_MATRIX_BAND_SPIRAL_VAL
+#   define ENABLE_RGB_MATRIX_CYCLE_ALL
+#   define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+#   define ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
+#   define ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+#   define ENABLE_RGB_MATRIX_CYCLE_OUT_IN
+#   define ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+#   define ENABLE_RGB_MATRIX_CYCLE_PINWHEEL
+#   define ENABLE_RGB_MATRIX_CYCLE_SPIRAL
+#   define ENABLE_RGB_MATRIX_DUAL_BEACON
+#   define ENABLE_RGB_MATRIX_RAINBOW_BEACON
+#   define ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+#   define ENABLE_RGB_MATRIX_RAINDROPS
+#   define ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+#   define ENABLE_RGB_MATRIX_HUE_BREATHING
+#   define ENABLE_RGB_MATRIX_HUE_PENDULUM
+#   define ENABLE_RGB_MATRIX_HUE_WAVE
+#   define ENABLE_RGB_MATRIX_PIXEL_RAIN
+#   define ENABLE_RGB_MATRIX_PIXEL_FLOW
+#   define ENABLE_RGB_MATRIX_PIXEL_FRACTAL
+// enabled only if RGB_MATRIX_FRAMEBUFFER_EFFECTS is defined
+#   define ENABLE_RGB_MATRIX_TYPING_HEATMAP
+#   define ENABLE_RGB_MATRIX_DIGITAL_RAIN
+// enabled only of RGB_MATRIX_KEYPRESSES or RGB_MATRIX_KEYRELEASES is defined
+#   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+#   define ENABLE_RGB_MATRIX_SOLID_REACTIVE
+#   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+#   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+#   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+#   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+#   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+#   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+#   define ENABLE_RGB_MATRIX_SPLASH
+#   define ENABLE_RGB_MATRIX_MULTISPLASH
+#   define ENABLE_RGB_MATRIX_SOLID_SPLASH
+#   define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
+#endif
+
+#ifdef UNDERGLOW_RGB_MATRIX_ENABLE
+#define UG_RGB_MATRIX_ANIMATIONS
+#endif
+
+/* Encoder */
+#ifdef ENCODER_ENABLE
+#    define ENCODERS_PAD_A { B11 }
+#    define ENCODERS_PAD_B { B10 }
+#    define ENCODER_RESOLUTION 4
+//#    define ENCODER_DIRECTION_FLIP  // 如果旋钮反向，启用此处
+#endif
+
+
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+/* Use 1000hz polling */
+#define USB_POLLING_INTERVAL_MS 1
+
+/* Because we use vibl, this has to be defined here instead of under keymaps/vial/config.h */
+/*使用vibl时在此定义 需要使用util下的vial_generate_keyboard_uid.py文件生成 */
+// #define VIAL_KEYBOARD_UID {0x02, 0x91, 0x79, 0x83, 0xDC, 0x8F, 0x38, 0x4D}
---
+++ keyboards/hbk_official/kuma980/rules.mk
@@ -0,0 +1,25 @@
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes      # 开启boot按键(Enable Bootmagic Lite)
+MOUSEKEY_ENABLE = yes       # 模拟鼠标 (Mouse keys)
+EXTRAKEY_ENABLE = yes       # 音频控制和系统控制 (Audio control and System control)
+
+
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend 灯光休眠
+
+NKRO_ENABLE = yes           # 全键无冲
+
+
+# NO
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+MIDI_ENABLE = no            # MIDI 控制器
+UNICODE_ENABLE = no         # Unicode
+BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+AUDIO_ENABLE = no           # Audio output on port C6
+FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+
